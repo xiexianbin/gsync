8cedcd446664d587f572ff284c1fd11f1823762e
xiexianbin <me@xiexianbin.cn>
2020-07-05 22:38:10 +0800

    feature: init gitee api v5

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # gsync lib
 
 gsync is golang sync lib, support aliyun oss/qiniu Kodo and so on.
+
+## install
+
+```
+dep ensure
+https_proxy=http://127.0.0.1:8001 http_proxy=http://127.0.0.1:8001 go mod vendor
+```
--- git/gitee/gitee.go
+++ git/gitee/gitee.go
@@ -1,1 +1,31 @@
 package gitee
+
+import (
+	"context"
+	"github.com/antihax/optional"
+	"golang.org/x/oauth2"
+
+	"gitee.com/openeuler/go-gitee/gitee"
+)
+
+// list Organizations
+func Organizations(user, accessToken string) ([]gitee.Group, error){
+	// oauth
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+
+	// configuration
+	giteeConf := gitee.NewConfiguration()
+	giteeConf.HTTPClient = oauth2.NewClient(ctx, ts)
+	// git client
+	giteeClient := gitee.NewAPIClient(giteeConf)
+	opt := &gitee.GetV5UsersUsernameOrgsOpts{
+		AccessToken: optional.NewString(accessToken),
+		PerPage: optional.NewInt32(1000),
+	}
+	groups, _, err := giteeClient.OrganizationsApi.GetV5UsersUsernameOrgs(ctx, user, opt)
+
+	return groups, err
+}
